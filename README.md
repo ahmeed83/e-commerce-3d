4a8acffbaa6f207ee0eceec89b4a9196ac1e4a05
Ahmed Aziz <ahmed.aziz@ing.com>
2020-05-30 10:14:15 +0200

    Replace amazon credential to the property file.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # webshop3d
 
 Webstore application 3D Electroincs 
+
+* start docker with:  `docker run --name webshop3d -p 5432:5432 -e POSTGRES_PASSWORD=docker3d -d postgres`
--- src/main/java/com/baghdadfocusit/webshop3d/configuration/aws/AmazonConfig.java
+++ src/main/java/com/baghdadfocusit/webshop3d/configuration/aws/AmazonConfig.java
@@ -5,19 +5,28 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonConfig {
 
-  @Bean
-  public AmazonS3 s3() {
-    AWSCredentials awsCredentials = new BasicAWSCredentials("AKIAJVYTYRKC2AVP7NZA",
-                                                            "LbKLpmtvOvYQUyds9zYsxQspi2ztt2EDO8gojlaT");
-    return AmazonS3ClientBuilder.standard()
-                                .withRegion("eu-central-1")
-                                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-                                .build();
-  }
+    @Value("${aws.access_key_id}")
+    private String accessKeyId;
+
+    @Value("${aws.secret_access_key}")
+    private String secretAccessKey;
+
+    @Value("${aws.s3.region}")
+    private String region;
+
+    @Bean
+    public AmazonS3 s3() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials(accessKeyId, secretAccessKey);
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
 }
